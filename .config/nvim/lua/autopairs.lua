60f3e0ea5dde9594218f34cecf20b675204b5e30
Daiki48 <daiki48.engineer@gmail.com>
2022-08-07 00:06:04 +0900

    nvim-autopairs update & vimdoc-ja and dein-ui is moving lazy

--- .config/nvim/lua/autopairs.lua
+++ .config/nvim/lua/autopairs.lua
@@ -18,6 +18,32 @@ npairs.add_rules({
   Rule("%", "%", "lua")
     :with_pair(ts_conds.is_ts_node({'string','comment'})),
   Rule("$", "$", "lua")
-    :with_pair(ts_conds.is_not_ts_node({'function'}))
+    :with_pair(ts_conds.is_not_ts_node({'function'})),
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+  Rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+  Rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']'),
+	Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
+			:use_regex(true)
+			:set_end_pair_length(2),
 })
 
