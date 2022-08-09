163c39ca260f09e8eb37bf3f40b40b14a546acd1
Daiki48 <daiki48.engineer@gmail.com>
2022-08-09 21:43:01 +0900

    encoding setting is add

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,4 +1,9 @@
 -- options おすすめ
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
 vim.o.helplang = 'ja,en'
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -12,10 +17,10 @@ vim.bo.smartindent = false
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.bo.autoread = true
-vim.wo.number = true
-vim.wo.relativenumber = true
+-- vim.wo.number = true
+-- vim.wo.relativenumber = true
 vim.wo.signcolumn = 'yes'
-vim.wo.cursorline = true
+vim.o.cursorline = true
 vim.cmd 'set clipboard+=unnamedplus'
 vim.cmd 'set wildoptions=pum'
 vim.cmd[[colorscheme coolnessFlair]]
