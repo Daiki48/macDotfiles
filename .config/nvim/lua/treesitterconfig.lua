b311c7b834b2c3f5b4f7732990b62670b9768079
Daiki48 <daiki48.engineer@gmail.com>
2022-08-11 00:23:27 +0900

    treesitterconfig update

--- .config/nvim/lua/treesitterconfig.lua
+++ .config/nvim/lua/treesitterconfig.lua
@@ -1,46 +1,40 @@
 require('nvim-treesitter.configs').setup {
-	-- ensure_installed = {
-	-- 	"lua",
-	-- 	"vim",
-	-- 	"typescript",
-	-- 	"toml",
-	-- 	"javascript",
-	-- 	"rust",
-	-- 	"css",
-	-- 	"json",
-	-- 	"yaml",
-	-- },
+	ensure_installed = {
+		"html",
+		"lua",
+		"vim",
+		"typescript",
+		"toml",
+		"javascript",
+		"rust",
+		"css",
+		"json",
+		"yaml",
+	},
 	-- highlight = {
 	-- 	enable = true,
 	-- },
-	-- sync_install = false,
-	-- yati = {
-	-- 	enable = true,
-	-- },
-  -- autotag = {
-	--  	enable = true,
-	-- -- 	filetypes = {
-      -- -- 'html',
-	-- -- 		'javascript',
-	-- -- 		'typescript',
-	-- -- 		'javascriptreact',
-	-- -- 		'typescriptreact',
-	-- -- 		'tsx',
-	-- -- 		'jsx',
-	-- -- 		'markdown',
-	-- -- 		'glimmer',
-	-- -- 		'handlebars',
-	-- -- 		'hbs'
-    -- -- },
-    -- -- skip_tags = {
-      -- -- 'br',
-	-- -- 		'img',
-	-- -- 		'input',
-	-- -- 		'meta',
-	-- -- 		'link',
-	-- -- 		'source'
-    -- -- },
-	--  },
+	indent = {
+		disable = true
+	},
+	sync_install = false,
+	yati = {
+		enable = true,
+	},
+  autotag = {
+	 	enable = true,
+		filetypes = {
+			'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
+			'xml',
+			'php',
+			'markdown',
+			'glimmer','handlebars','hbs'
+		},
+		skip_tags = {
+			'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'slot',
+			'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr','menuitem'
+		}
+	 },
 }
 
 require("nvim-ts-autotag").setup()
