bb02c7d528bd3e42ceabf1fa95578819fe7f2555
Daiki48 <daiki48.engineer@gmail.com>
2022-08-12 00:09:21 +0900

    nvim-autopairs config is update

--- .config/nvim/lua/autopairs.lua
+++ .config/nvim/lua/autopairs.lua
@@ -1,49 +1,50 @@
-local npairs = require("nvim-autopairs")
-local Rule = require('nvim-autopairs.rule')
+require('nvim-autopairs').setup{}
 
-npairs.setup({
-    check_ts = true,
-    ts_config = {
-        lua = {'string'},-- it will not add a pair on that treesitter node
-        javascript = {'template_string'},
-        java = false,-- don't check treesitter on java
-    }
-})
+-- local npairs = require("nvim-autopairs")
+-- local Rule = require('nvim-autopairs.rule')
 
-local ts_conds = require('nvim-autopairs.ts-conds')
+-- npairs.setup({
+--     check_ts = true,
+--     ts_config = {
+--         lua = {'string'}, -- it will not add a pair on that treesitter node
+--         javascript = {'template_string'},
+--         java = false, -- don't check treesitter on java
+--     }
+-- })
 
+-- local ts_conds = require('nvim-autopairs.ts-conds')
 
--- press % => %% only while inside a comment or string
-npairs.add_rules({
-  Rule("%", "%", "lua")
-    :with_pair(ts_conds.is_ts_node({'string','comment'})),
-  Rule("$", "$", "lua")
-    :with_pair(ts_conds.is_not_ts_node({'function'})),
-  Rule(' ', ' ')
-    :with_pair(function (opts)
-      local pair = opts.line:sub(opts.col - 1, opts.col)
-      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
-    end),
-  Rule('( ', ' )')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%)') ~= nil
-      end)
-      :use_key(')'),
-  Rule('{ ', ' }')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%}') ~= nil
-      end)
-      :use_key('}'),
-  Rule('[ ', ' ]')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%]') ~= nil
-      end)
-      :use_key(']'),
-	Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
-			:use_regex(true)
-			:set_end_pair_length(2),
-})
+-- -- press % => %% only while inside a comment or string
+-- npairs.add_rules({
+--   Rule("%", "%", "lua")
+--     :with_pair(ts_conds.is_ts_node({'string','comment'})),
+--   Rule("$", "$", "lua")
+--     :with_pair(ts_conds.is_not_ts_node({'function'})),
+--   Rule(' ', ' ')
+--     :with_pair(function (opts)
+--       local pair = opts.line:sub(opts.col - 1, opts.col)
+--       return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+--     end),
+--   Rule('( ', ' )')
+--       :with_pair(function() return false end)
+--       :with_move(function(opts)
+--           return opts.prev_char:match('.%)') ~= nil
+--       end)
+--       :use_key(')'),
+--   Rule('{ ', ' }')
+--       :with_pair(function() return false end)
+--       :with_move(function(opts)
+--           return opts.prev_char:match('.%}') ~= nil
+--       end)
+--       :use_key('}'),
+--   Rule('[ ', ' ]')
+--       :with_pair(function() return false end)
+--       :with_move(function(opts)
+--           return opts.prev_char:match('.%]') ~= nil
+--       end)
+--       :use_key(']'),
+-- 	Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
+-- 			:use_regex(true)
+-- 			:set_end_pair_length(2),
+-- })
 
