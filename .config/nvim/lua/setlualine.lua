b379367c625a9c1de5ffec5c1579469198aa6ad9
Daiki48 <daiki48.engineer@gmail.com>
2022-08-11 22:22:47 +0900

    theme is delete

--- .config/nvim/lua/setlualine.lua
+++ .config/nvim/lua/setlualine.lua
@@ -52,7 +52,7 @@ end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'iceberg_dark',
+--    theme = 'iceberg_dark',
     component_separators = {left = '', right = ''},
     section_separators = {left = '', right = ''},
     disabled_filetypes = {}
