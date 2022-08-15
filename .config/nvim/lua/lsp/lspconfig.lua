1535102fa91638ccdd843b1fa2b62c35d1efdf67
Daiki48 <daiki48.engineer@gmail.com>
2022-08-15 21:12:57 +0900

    [update] mason-lspconfig

--- .config/nvim/lua/lsp/lspconfig.lua
+++ .config/nvim/lua/lsp/lspconfig.lua
@@ -18,7 +18,68 @@ mason.setup({
 local mason_lspconfig = require('mason-lspconfig')
 
 mason_lspconfig.setup_handlers({ function(server_name)
+	local node_root_dir = nvim_lsp.util.root_pattern("package.json")
+--	local is_node_repo = node_root_dir(vim.api.nvim_buf_set_name(0)) ~= nil
+
   local opts = {}
+
+	if lsp_capabilities ~= nil then
+		opts.capabilities = lsp_capabilities
+	end
+
+	if server_name == "tsserver" then
+		-- if not is_node_repo then
+		-- 	return
+		-- end
+		opts.settings = { documentFormatting = false }
+	elseif server_name == "eslint" then
+		-- if not is_node_repo then
+		-- 	return
+		-- end
+		opts.root_dir = node_root_dir
+	elseif server_name == "denols" then
+		-- if is_node_repo then
+		-- 	return
+		-- end
+		opts.root_dir = nvim_lsp.util.root_pattern(
+			"deno.json",
+			"deno.jsonc",
+			"deps.ts",
+			"import_map.json"
+		)
+		opts.init_options = {
+			lint = true,
+			unstable = true,
+			suggest = {
+				imports = {
+					hosts = {
+						["https://deno.land"] = true,
+						["https://cdn.nest.land"] = true,
+						["https://crux.land"] = true
+					}
+				}
+			}
+		}
+	elseif server_name == "tailwindcss" then
+		opts.root_dir = nvim_lsp.util.root_pattern("tailwind.config.js")
+	elseif server_name == "sumneko_lua" then
+		local path = {
+			"?.lua",
+			"?/init.lua"
+		}
+		opts.settings = {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+					path = path,
+				},
+				completion = { callSnippet = "Replace" },
+				diagnostics = { globals = { 'vim', 'lsp_capabilities' } },
+				workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+				telemetry = { enable = false },
+			}
+		}
+  end
 
   -- opts.on_attach = function(_, bufnr)
   --   local buopts = { silent = true, buffer = bufnr }
@@ -27,14 +88,6 @@ mason_lspconfig.setup_handlers({ function(server_name)
   --   vim.keymap.set('n', 'grf', buf.references, bufopts)
   --   vim.keymap.set('n', '<space>p', buf.format, bufopts)
   -- end
-
-  if server_name == "sumneko_lua" then
-	opts.settings = {
-		Lua = {
-			diagnostics = { globals = { 'vim' } },
-		}
-	}
-  end
 
 
   nvim_lsp[server_name].setup(opts)
