40cb620bb68b1820162f5d23157811c536c8d90b
Daiki48 <daiki48.engineer@gmail.com>
2022-08-15 12:55:53 +0900

    brew shellenv is setting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-	eval (/opt/homebrew/bin/brew -c)
-  hs.ipc.cliInstall("/opt/homebrew")
+	eval (/opt/homebrew/bin/brew shellenv)
 
 	function fish_user_key_bindings
  		 bind \cr 'peco_select_history (commandline -b)'
@@ -16,6 +14,8 @@ set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 set -gx PATH "$HOME/tools/lua-language-server/bin/macOS:$PATH"
 set -gx MOCWORD_DATA "$HOME/path/to/mocword.sqlite"
+
+set -U FZF_LEGACY_KEYBINDINGS 0
 
 alias lazygit='lazygit -ucd ~/.config/lazygit'
 
