e18ab1cfdb02dcffd648ff1d26663979579b3e5c
Daiki48 <daiki48.engineer@gmail.com>
2022-08-20 14:01:04 +0900

    [update] nvim-ts-autotag enable

--- .config/nvim/lua/treesitter/autotag.lua
+++ .config/nvim/lua/treesitter/autotag.lua
@@ -2,26 +2,3 @@ local status, autotag = pcall(require, "nvim-ts-autotag")
 if (not status) then return end
 
 autotag.setup({})
-
--- local autotag = require('nvim-ts-autotag')
--- if not autotag then
--- 	return
--- end
-
--- local	filetypes = {
---     'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
---     'xml',
---     'php',
---     'markdown',
---     'glimmer','handlebars','hbs'
--- }
-
--- local skip_tags = {
---   'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'slot',
---   'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr','menuitem'
--- }
-
--- autotag.setup({
--- 	filetypes = filetypes,
--- 	skip_tags = skip_tags
--- })
