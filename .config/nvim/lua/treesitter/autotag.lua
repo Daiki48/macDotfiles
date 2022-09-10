94a785469f69cdfe3ab4afe9d44af4d1bc64c541
Daiki48 <daiki48.engineer@gmail.com>
2022-09-10 17:54:50 +0900

    [update] filetypes change

--- .config/nvim/lua/treesitter/autotag.lua
+++ .config/nvim/lua/treesitter/autotag.lua
@@ -2,5 +2,13 @@ local status, autotag = pcall(require, "nvim-ts-autotag")
 if (not status) then return end
 
 autotag.setup({
-	filetypes = { "rust", "tsx" }
+	filetypes = {
+		"rust",
+		"tsx",
+		"html",
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		"javascriptreact",
+	}
 })
